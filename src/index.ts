2b491a4713d34b640ab6bb41f2bd2b03d92c3743
vaishnav <vaishnav240204@gmail.com>
2022-11-06 14:27:16 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -10,24 +10,35 @@
     });
 };*/
 
-import tlds from "./tlds.js";
+import { tlds } from "./tlds.json";
 const main = async (text: string): Promise<string[] | null> => {
+
+  text = text.replace("\n", " ").replace("\r", " ").replace("\t", "    ").replace("\u200b", " ").trim();
+
+  console.log({text})
+
   const regex = new RegExp(
     `((https://)|(http://))?(([0-z|\\-]{1,63})[.])?([0-z|\\-]{1,63})[.](${tlds.join(
       "|"
     )})(\/[0-z|\/|&|%|\\-|_|.|+|=|~|:|;|?|#|@]{1,2048})?`,
-    "g"
+    "gui"
   );
 
   const matches: string[] = text?.match(regex) || [];
+
+  matches.forEach((match, index) => {
+    if (match.startsWith("http://") || match.startsWith("https://")) {
+      matches[index] = match;
+    } else {
+      matches[index] = "https://" + match;
+    }
+  });
 
   console.log({ matches });
 
   return matches;
 };
 
-main(
-  "Picking out google.com is trivial. As well as YOUTUBE.com,\nand even knows what is a real tld, and what is not.a.real.top-level-domain!"
-);
+main("https://google.com").then((res) => console.log(res));
 
 export default main;
